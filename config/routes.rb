0e1d8968d39a67f56d506cc62a0c28b74ecf0baa
Jacques Mesnet <jacquesmesnet@gmail.com>
2018-02-12 16:14:57 +0100

    NavBar done ! -> Sidenav

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # ROOT
+  authenticated :user do
+    root to: "pages#home", as: :authenticated_root
+  end
+  unauthenticated do
+    root 'pages#landing'
+  end
+
+end
